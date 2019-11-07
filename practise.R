316d39f61913229fd5860ea640cbd0a078760980
yinshu zhang <yinshu.zhang.git@gmail.com>
2019-11-06 23:58:11 -0500

    finish n gram

--- practise.R
+++ practise.R
@@ -7,7 +7,7 @@ text <- c("Because I could not stop for Death -",
           "and Immortality")
 text_df <- tibble(line = 1:4, text = text)
 text_df
-text_df %>% unnest_tokens(word, text)
+text_df %>% unnest_tokens(word, text) # "word" is the output column name, "text" is input column name of text_df
 
 # get a book
 library(janeaustenr)
@@ -15,7 +15,7 @@ library(stringr)
 original_books <- austen_books() %>%
   group_by(book) %>%
   mutate(linenumber = row_number(),
-         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",  # DIVCLC, roman number?
                                                  ignore_case = TRUE)))) %>%
   ungroup()
 
@@ -26,42 +26,75 @@ tidy_books
 # remove stop words
 data("stop_words")
 tidy_books <- tidy_books %>% anti_join(stop_words)
+tidy_books
 
 # word fequence
 tidy_books %>% count(word, sort=T)
+# plot
+library(ggplot2)
+tidy_books %>% count(word, sort=T) %>%
+  filter(n>600) %>%
+  mutate(word=reorder(word, n)) %>% # sort column by "n"
+  ggplot(aes(word,n)) + geom_col() + coord_flip()
+
 
 # wordcloud
 library(wordcloud)
 tidy_books %>% anti_join(stop_words) %>% count(word) %>% with(wordcloud(word, n, max.words = 100))
 
+# ---------------------
+# chapt2 opinion mining/sentiment analysis
 ### uni-gram/sentiment
 sentiments
 # three types of lexicon
+library(tidytext)
+library(textdata)
 
-get_sentiments('afinn')
-get_sentiments('bing')
-get_sentiments('nrc')
+# most english words are neutral
+get_sentiments('nrc')   # categorized
+get_sentiments('afinn') # from -5 to 5 scale of negative to positive
+get_sentiments('bing')  # binary negative positive
+
 # different expression of sentiments
-get_sentiments('nrc') %>% group_by(sentiment) %>% summarise(n())
+# get_sentiments('nrc') %>% group_by(sentiment) %>% summarise(n())
 
-# multi word sentiment
-bingnegative <- get_sentiments("bing") %>% 
-  filter(sentiment == "negative")
+# 
+library(janeaustenr)
+library(dplyr)
+library(stringr)
 
-wordcounts <- tidy_books %>%
-  group_by(book, chapter) %>%
-  summarize(words = n())
+# what's positive word count of book "Emma"
+tidy_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
+                                                 ignore_case = T)))) %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
 
-tidy_books %>%
-  semi_join(bingnegative) %>%
-  group_by(book, chapter) %>%
-  summarize(negativewords = n()) %>%
-  left_join(wordcounts, by = c("book", "chapter")) %>%
-  mutate(ratio = negativewords/words) %>%
-  filter(chapter != 0) %>%
-  top_n(1) %>%
-  ungroup()
+nrc_joy <- get_sentiments("nrc") %>% filter(sentiment == 'joy')
 
+tidy_books %>% filter(book == "Emma") %>% inner_join(nrc_joy) %>% count(word, sort=T)
+
+# postive/negative changing trend over each book
+library(tidyr)
+jane_austen_sentiment <- tidy_books %>%
+  inner_join(get_sentiments('bing')) %>%
+  count(book, index=linenumber %/% 80, sentiment) %>% # for each 80 lines, %/% is floor, count number of sentiment
+  spread(sentiment, n, fill = 0) %>%                  # read  https://r4ds.had.co.nz/tidy-data.html explain gather and spread, spread basically turn given value of column into columns, in this case "postive" and "negative" are values of "sentiment", spread turn to columns
+  mutate(sentiment = positive-negative) 
+
+ggplot(jane_austen_sentiment, aes(index, sentiment, fill=book)) +
+  geom_col(show.legend = F) +
+  facet_wrap(~book, ncol=2, scales="free_x")
+
+# beyond uni-gram, tokenize with sentence
+tibble(text=prideprejudice) %>% unnest_tokens(sentence, text, token="sentences")
+# not good at non-ASC
+
+# ----------------------- 
+# chapter 3, word frequency
+# not interested atm
 # tf-idf, intended to measure how important a word is to a document
 # inverse document frequency (idf), which decreases the weight for commonly used words and increases the weight for words 
 # that are not used very much in a collection of documents. 
@@ -86,7 +119,9 @@ book_words %>% bind_tf_idf(word, book, n )
 # important word
 book_words %>% bind_tf_idf(word, book, n ) %>% arrange(desc(tf_idf))
 
-# n-gram
+# ----------------------
+# charpter 4, n-gram
+# 2-gram tokenizing
 austen_bigrams <- austen_books() %>%
   unnest_tokens(bigram, text, token = "ngrams", n = 2)
 
